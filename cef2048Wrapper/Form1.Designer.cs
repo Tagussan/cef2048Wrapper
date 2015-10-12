c04f88b5cdce8e29bc41ed4ca5c86423cdfb8e8b
Tagussan <jap.yoshi@gmail.com>
2015-10-12 11:59:54 +0900

    loading local resource success

--- cef2048Wrapper/Form1.Designer.cs
+++ cef2048Wrapper/Form1.Designer.cs
@@ -30,10 +30,12 @@
         {
             this.pnlBrowser = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.debugText = new System.Windows.Forms.TextBox();
+            this.serialText = new System.Windows.Forms.TextBox();
             this.cmbPortName = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.browserConsole = new System.Windows.Forms.TextBox();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,20 +47,58 @@
             this.pnlBrowser.AutoSize = true;
             this.pnlBrowser.Location = new System.Drawing.Point(12, 12);
             this.pnlBrowser.Name = "pnlBrowser";
-            this.pnlBrowser.Size = new System.Drawing.Size(734, 697);
+            this.pnlBrowser.Size = new System.Drawing.Size(606, 513);
             this.pnlBrowser.TabIndex = 0;
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.textBox2);
-            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.debugText);
+            this.panel2.Controls.Add(this.serialText);
             this.panel2.Controls.Add(this.cmbPortName);
             this.panel2.Controls.Add(this.label1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel2.Location = new System.Drawing.Point(764, 0);
+            this.panel2.Location = new System.Drawing.Point(636, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(459, 724);
+            this.panel2.Size = new System.Drawing.Size(459, 641);
             this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(217, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // debugText
+            // 
+            this.debugText.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.debugText.Location = new System.Drawing.Point(20, 284);
+            this.debugText.Multiline = true;
+            this.debugText.Name = "debugText";
+            this.debugText.Size = new System.Drawing.Size(427, 342);
+            this.debugText.TabIndex = 3;
+            // 
+            // serialText
+            // 
+            this.serialText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.serialText.Location = new System.Drawing.Point(20, 63);
+            this.serialText.Multiline = true;
+            this.serialText.Name = "serialText";
+            this.serialText.Size = new System.Drawing.Size(427, 215);
+            this.serialText.TabIndex = 2;
+            // 
+            // cmbPortName
+            // 
+            this.cmbPortName.FormattingEnabled = true;
+            this.cmbPortName.Location = new System.Drawing.Point(75, 10);
+            this.cmbPortName.Name = "cmbPortName";
+            this.cmbPortName.Size = new System.Drawing.Size(121, 26);
+            this.cmbPortName.TabIndex = 1;
             // 
             // label1
             // 
@@ -70,38 +110,22 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Port";
             // 
-            // cmbPortName
+            // browserConsole
             // 
-            this.cmbPortName.Enabled = false;
-            this.cmbPortName.FormattingEnabled = true;
-            this.cmbPortName.Location = new System.Drawing.Point(75, 10);
-            this.cmbPortName.Name = "cmbPortName";
-            this.cmbPortName.Size = new System.Drawing.Size(121, 26);
-            this.cmbPortName.TabIndex = 1;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.textBox1.Location = new System.Drawing.Point(20, 42);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(427, 319);
-            this.textBox1.TabIndex = 2;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.textBox2.Location = new System.Drawing.Point(20, 367);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(427, 342);
-            this.textBox2.TabIndex = 3;
+            this.browserConsole.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.browserConsole.Location = new System.Drawing.Point(12, 531);
+            this.browserConsole.Multiline = true;
+            this.browserConsole.Name = "browserConsole";
+            this.browserConsole.Size = new System.Drawing.Size(618, 98);
+            this.browserConsole.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1223, 724);
+            this.ClientSize = new System.Drawing.Size(1095, 641);
+            this.Controls.Add(this.browserConsole);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.pnlBrowser);
             this.Name = "Form1";
@@ -120,8 +144,10 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.ComboBox cmbPortName;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox debugText;
+        private System.Windows.Forms.TextBox serialText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox browserConsole;
     }
 }
 
--- cef2048Wrapper/Form1.cs
+++ cef2048Wrapper/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,29 +10,96 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using CefSharp;
 using CefSharp.WinForms;
+using CefSharp.Internals;
 using System.IO.Ports;
+using System.IO;
+using System.Reflection;
 
 namespace cef2048Wrapper
 {
     public partial class Form1 : Form
     {
         private ChromiumWebBrowser browser;
+        private Boolean running;
+        private SerialPort serial;
+        private static String receivedData;
+        private static readonly string _myPath = Application.StartupPath;
+        private static readonly string _pagesPath = Path.Combine(_myPath, "pages");
+        private string GetPagePath(string pageName)
+        {
+            return Path.Combine(_pagesPath, pageName);
+        }
         public Form1()
         {
             InitializeComponent();
-            browser = new ChromiumWebBrowser("https://gabrielecirulli.github.io/2048/")
-            {
-                Dock = DockStyle.Fill
-            };
-            pnlBrowser.Controls.Add(browser);
+            initBrowser();
             var portList = SerialPort.GetPortNames();
             cmbPortName.Items.Clear();
             cmbPortName.Items.AddRange(portList);
+            receivedData = "";
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+
+        private void initSerial()
+        {
+            serial = new SerialPort(cmbPortName.Text);
+            serial.BaudRate = 115200;
+            serial.Parity = Parity.None;
+            serial.StopBits = StopBits.One;
+            serial.DataBits = 8;
+            serial.Handshake = Handshake.None;
+            serial.RtsEnable = false;
+            serial.DataReceived += new SerialDataReceivedEventHandler(serialReceiveHandler);
+            serial.Open();
+        }
+
+        private void initBrowser()
+        {
+            browser = new ChromiumWebBrowser(GetPagePath("index.html"))
+            {
+                Dock = DockStyle.Fill
+            };
+            pnlBrowser.Controls.Add(browser);
+            browser.ConsoleMessage += new EventHandler<ConsoleMessageEventArgs>(consoleReceiveHandler);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                //end process
+                button1.Text = "Start";
+                running = false;
+            }
+            else if (!running && cmbPortName.Text != "")
+            {
+                //start process
+                button1.Text = "Stop";
+                running = true;
+                initSerial();
+
+            }
+        }
+
+        private void serialReceiveHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            receivedData += indata;
+        }
+
+        private void consoleReceiveHandler(object sender, ConsoleMessageEventArgs e)
+        {
+            this.Invoke((Action<object>)((obj) =>
+            {
+                browserConsole.AppendText(e.Message + "\n");
+            }), new Object());
+        }
+
     }
 }
+    
